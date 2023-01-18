10567915ffe68638c6f271e73906aa57bd4db41f
shui <syui56@icloud.com>
2023-01-18 18:29:07 +0900

    closes #1

--- cstomp3.py
+++ cstomp3.py
@@ -8,26 +8,22 @@ from selenium.webdriver.chrome.options import Options
 import time
 import os
 
-count = 0
-
 CHROMEDRIVER = "C:\data\etc\chromedriver.exe"
 chrome_service = fs.Service(executable_path=CHROMEDRIVER)
-
 options = webdriver.ChromeOptions()
 browser = webdriver.Chrome(options=options)
-
 browser.get('https://soundoftext.com/')
 
+fileobj = open('sentence.txt', 'r')
 textBox = browser.find_element(By.XPATH, '//*[@id="app"]/div[2]/div/form/div[1]/div/textarea')
 submit = browser.find_element(By.XPATH, '//*[@id="app"]/div[2]/div/form/div[3]/input')
-
-fileobj = open('sentence.txt', 'r')
+countLine = 0
 
 while True:
     line = fileobj.readline()
     if line:
      textBox.send_keys(line)
-     count += 1
+     countLine += 1
      submit.click()
     else:
         break
@@ -35,13 +31,10 @@ while True:
 #ダウンロードボタンが生成されるまで待機
 time.sleep(3)
 
-browser.execute_script("window.scrollTo(600, 1000);")
-download = browser.find_elements(By.XPATH, "//*[@id='app']/div[4]/div/div[*]/div[2]/a[2]")
-
 browser.execute_script("window.scrollTo(600, 1500);")
 download = browser.find_elements(By.XPATH, "//*[@id='app']/div[4]/div/div[*]/div[2]/a[2]")
 
-for i in range(count):
+for i in range(countLine):
    
     browser.execute_script('arguments[0].click();', download[i])
 
